ac37717784924336baafbd5c5e572b536b002605
crazedrunner <joshua@crazedrunner.com>
2020-04-23 00:55:50 -0400

    defining base class for mod...  Working on structure.

---
+++ src/main/java/com/crazedrunner/modularminer/common/ModularMiner.java
@@ -0,0 +1,22 @@
+package com.crazedrunner.modularminer.common;
+
+// Imports go here
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("modularminer")
+public final class ModularMiner{
+
+}
